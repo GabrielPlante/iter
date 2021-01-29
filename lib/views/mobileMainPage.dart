d039c090697805c69a8a536c232238665658ef74
Charles Roger <charles.roger@etu.unice.fr>
2021-01-29 08:42:03 +0100

    adding same view as web but for mobile

--- lib/views/mobileMainPage.dart
+++ lib/views/mobileMainPage.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:iter/models/quiz.dart';
+import 'package:iter/services/databaseService.dart';
+
+import 'components/quizCardComponentForMobile.dart';
 
 class MobileMainPage extends StatefulWidget {
   @override
-  _MobileMainPageState createState() => _MobileMainPageState();
+  MobileMainPageState createState() => MobileMainPageState();
 }
 
-class _MobileMainPageState extends State<MobileMainPage> {
+class MobileMainPageState extends State<MobileMainPage> {
+  final DatabaseService _databaseService = DatabaseService();
+  List<Quiz> quizs = [];
+  String quizChosen = '';
+
+  @override
+  void initState() {
+    initQuizs();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Vous êtes sur la version mobile !"),
-      ),
+    return Scaffold(
+        appBar: AppBar(title: Text('Menu Principal')),
+        body: quizs.isEmpty ? Text(" Nothing") :
+        ListView.builder(
+            itemCount: quizs.length,
+            itemBuilder: (context, index) {
+              return QuizCardComponentForMobile(quiz: quizs[index],quizJoined : quizs[index].id == quizChosen, parent: this );
+            })
     );
+  }
+
+
+  void initQuizs() async {
+    List<Quiz> result = await  _databaseService.allQuiz;
+    setState(() {
+      quizs = result;
+    });
+
+  }
+
+  void updateQuizChoice(String quizId) async {
+    if(quizChosen == quizId){
+      await _databaseService.removePlayer(quizId);
+      setState(() {
+        quizChosen = '';
+      });
+    } else {
+      if(quizChosen != '') await _databaseService.removePlayer(quizChosen);
+      await _databaseService.addPlayer(quizId);
+      setState(() {
+        quizChosen = quizId;
+      });
+    }
   }
 }
